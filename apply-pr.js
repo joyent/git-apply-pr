9fcb1005f566267aa96d0d912ff862d959e462dc
Milos Sakovic <m@deva.co>
2015-12-02 16:29:53 +0100

    Output error message if email not set.

--- apply-pr.js
+++ apply-pr.js
@@ -2,19 +2,19 @@
 
 /*
  * The MIT License (MIT)
- * 
+ *
  * Copyright (c) 2015 Timothy J Fontaine <tjfontaine@gmail.com>
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -147,6 +147,10 @@ function resolveNames() {
   function resolve(err, name) {
     if (err) {
       exitWithMsg(err);
+    }
+
+    if (!name.email) {
+      exitWithMsg(util.format('Email not set for %s', name.name));
     }
 
     result.push(util.format('%s <%s>', name.name, name.email));
